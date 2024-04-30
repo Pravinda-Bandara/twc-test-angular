77abd153b2b32b50c5338f9b65b0ca29ec436ac2
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-04-30 22:27:39 +0530

    implement login and register pages

--- src/app/view/login/login.component.ts
+++ src/app/view/login/login.component.ts
@@ -1,14 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from "../../service/user.service";
+import { userResponse } from "../../types/user-types";
+import { StoreService } from "../../service/store.service";
+import { Router } from "@angular/router";
+import { UserValidationUtil } from "../../util/userValidationUtil";
+import { take } from "rxjs";
 
 @Component({
   selector: 'app-login',
   template: `
-    <p>
-      login works!
-    </p>
-  `,
-  styles: ``
-})
-export class LoginComponent {
+      <div class="flex justify-evenly items-center h-screen bg-fixed-cover-2">
+          <div class="p-10">
+              <h1 class="text-9xl font-bold text-white mb-4">Hi there,</h1>
+              <p class="text-white text-2xl mb-10">
+                  Welcome to our <br /> contacts portal
+              </p>
+              <form>
+                  <div>
+                      <input type="text"
+                             placeholder="Enter user Name"
+                      [(ngModel)]="userName"
+                      name="userName" class="custom-input">
 
+                  </div>
+                  <div>
+                      <input type="text"
+                             placeholder="Enter password"
+                             [(ngModel)]="password"
+                             name="userName" class="custom-input">
+                  </div>
+                  <button (click)="onLogin()" class="custom-button w-1/4">Login</button>
+                  <span class="text-white mx-5"> or </span>
+                  <button
+                          class="underline text-white text-sm mr-7"
+                          type="button"
+                          (click)="navigateToRegister()"
+                  >
+                      Click here to Register
+                  </button>
+              </form>
+          </div>
+          <app-logo
+                  textColor="text-black"
+                  imageSize="w-20"
+                  textSize="text-5xl"
+          ></app-logo>
+      </div>
+  `,
+  styles: [
+    // Add Tailwind CSS styles here
+  ]
+})
+export class LoginComponent implements OnInit {
+  constructor(
+    private userService: UserService,
+    private storeService: StoreService,
+    private router: Router,
+    private userValidationUtil: UserValidationUtil
+  ) {}
+
+  ngOnInit() {
+    const userInfo = localStorage.getItem('userInfo');
+    if (userInfo) {
+      this.router.navigate(['app/contacts']);
+    }
+  }
+
+  userName: string = '';
+  password: string = '';
+
+  onLogin() {
+    console.log(this.userName,this.password)
+    if (!this.userValidationUtil.validate(this.userName, this.password)) {
+      return;
+    }
+
+    this.userService.login({ userName: this.userName, userPassword: this.password })
+      .pipe(take(1))
+      .subscribe(
+        (response: userResponse) => {
+          this.storeService.UserSignIn(response.userId);
+          localStorage.setItem('userInfo', JSON.stringify(response.userId));
+          this.router.navigate(['app/contacts']);
+        },
+        (error) => {
+          console.error('Login failed:', error);
+        }
+      );
+  }
+
+  navigateToRegister() {
+    this.router.navigate(['/register']);
+  }
 }
--- src/app/view/register/register.component.ts
+++ src/app/view/register/register.component.ts
@@ -1,14 +1,112 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from "../../service/user.service";
+import { StoreService } from "../../service/store.service";
+import { Router } from "@angular/router";
+import { UserValidationUtil } from "../../util/userValidationUtil";
+import { take } from "rxjs";
 
 @Component({
   selector: 'app-register',
   template: `
-    <p>
-      register works!
-    </p>
-  `,
-  styles: ``
-})
-export class RegisterComponent {
+      <div class="flex justify-evenly items-center h-screen bg-fixed-cover-2">
+          <div class="p-10">
+              <h1 class="font-bold text-5xl my-5 my-10 text-white">Register Now!</h1>
+              <form (ngSubmit)="onRegister()">
+                  <div>
+                      <input
+                              type="text"
+                              placeholder="e-mail"
+                              [(ngModel)]="userName"
+                              name="userName"
+                              class="custom-input"
+                      />
+                  </div>
+                  <div>
+                      <input
+                              type="password"
+                              placeholder="create password"
+                              [(ngModel)]="userPassword"
+                              name="userPassword"
+                              class="custom-input"
+                      />
+                  </div>
+                  <div>
+                      <input
+                              type="password"
+                              placeholder="confirm password"
+                              [(ngModel)]="confirmPassword"
+                              name="confirmPassword"
+                              class="custom-input"
+                      />
+                  </div>
 
+                  <div>
+                      <button type="submit" [disabled]="isPending" class="custom-button w-2/5">
+                          Register
+                      </button>
+                  </div>
+              </form>
+
+              <div>
+                  <button class="underline text-white text-lg" type="button" (click)="navigateToLogin()">
+                      &lt; Back To Login
+                  </button>
+              </div>
+          </div>
+
+          <div class="flex items-center">
+              <app-logo textColor="text-black" imageSize="w-20" textSize="text-5xl"></app-logo>
+          </div>
+      </div>
+  `,
+  styles: [],
+})
+export class RegisterComponent implements OnInit {
+  userName: string = '';
+  userPassword: string = '';
+  confirmPassword: string = '';
+  isPending: boolean = false;
+
+  constructor(
+    private userService: UserService,
+    private storeService: StoreService,
+    private router: Router,
+    private userValidationUtil: UserValidationUtil
+  ) {}
+
+  ngOnInit() {
+    const userInfo = localStorage.getItem('userInfo');
+    if (userInfo) {
+      this.router.navigate(['app/contacts']);
+    }
+  }
+
+  onRegister() {
+    if (!this.userValidationUtil.validate(this.userName, this.userPassword)) {
+      return;
+    }
+    if (this.userPassword !== this.confirmPassword) {
+      console.error('Passwords do not match');
+      return;
+    }
+
+    this.isPending = true;
+    this.userService.register({ userName: this.userName, userPassword: this.userPassword })
+      .pipe(take(1))
+      .subscribe(
+        (response: any) => {
+          this.storeService.UserSignIn(response.userId);
+          localStorage.setItem('userInfo', JSON.stringify(response.userId));
+          this.router.navigate(['app/welcome']);
+        },
+        (error) => {
+          console.error('Registration failed:', error);
+        }
+      )
+      .add(() => this.isPending = false);
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
