4a929a66981837111b8e8ecbbc1742e6e2b495b5
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-05-07 18:01:14 +0530

    create contact service

---
+++ src/app/service/contact.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {ContactRequest, ContactResponse} from "../types/contact-types";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+  private baseUrl = 'http://localhost:5050/api/v1/contacts/';
+
+  constructor(private http: HttpClient) { }
+
+  getContactList(contactId: string): Observable<ContactResponse[]> {
+    return this.http.get<ContactResponse[]>(`${this.baseUrl}/${contactId}`);
+  }
+
+  deleteContact(userId: string): Observable<{ message: string }> {
+    return this.http.delete<{ message: string }>(`${this.baseUrl}/${userId}`);
+  }
+
+  signup(user: ContactRequest): Observable<ContactResponse> {
+    return this.http.post<ContactResponse>(`${this.baseUrl}`, user);
+  }
+
+  updateContact(contact: ContactResponse): Observable<ContactResponse> {
+    return this.http.patch<ContactResponse>(`${this.baseUrl}/${contact._id}`, contact);
+  }
+}
+
