18895946b1324bdfe00b6221b04f84277e56d2cc
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-05-09 06:15:05 +0530

    delete button ui fix

--- src/app/component/delete-pop-up.component.ts
+++ src/app/component/delete-pop-up.component.ts
@@ -7,8 +7,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
           <div class="bg-white px-9 pb-2 pt-5 rounded-2xl shadow-md">
               <p class="text-lg mb-4">{{ message }}</p>
               <div class="flex justify-center">
-                  <button class="custom-button p-1 mb-3 mt-4 mr-2 border-customBlue" (click)="handleConfirm()">Yes</button>
-                  <button class="custom-button-white p-1 mb-3 mt-4" (click)="handleCancel()">Cancel</button>
+                  <button class="custom-button p-1 mb-3 mt-0 mr-2 border-customBlue" (click)="handleConfirm()">Yes</button>
+                  <button class="custom-button-white p-1 mb-3 mt-0" (click)="handleCancel()">Cancel</button>
               </div>
           </div>
       </div>
