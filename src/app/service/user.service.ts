397ea1e3e289653d564ccb4c36ef80cbecc2554d
Pravinda-Bandara <bandarapravinda@gmail.com>
2025-01-28 02:53:45 +0530

    use base url

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {userRequest, userResponse,} from "../types/user-types";
-
+import { userRequest, userResponse } from "../types/user-types";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private baseUrl = 'http://localhost:5050/api/v1/users';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(userRequest: userRequest): Observable<userResponse> {
-    return this.http.post<userResponse>('http://localhost:5050/api/v1/users/login', userRequest);
+    return this.http.post<userResponse>(`${this.baseUrl}/login`, userRequest);
   }
 
   register(userRequest: userRequest): Observable<userResponse> {
-    return this.http.post<userResponse>('http://localhost:5050/api/v1/users/register', userRequest);
+    return this.http.post<userResponse>(`${this.baseUrl}/register`, userRequest);
   }
 }
